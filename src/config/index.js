63b6868c29aa3a54d92a5db7f2c4ca811cbb681c
ZeroWhisper <marcos.devel@gmail.com>
2019-10-04 11:18:00 -0300

    Ajuste

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 
 const url =
   process.env.NODE_ENV != 'dev'
-    ? process.env.HEROKU_POSTGRESQL_PURPLE_URL
+    ? // ? process.env.HEROKU_POSTGRESQL_PURPLE_URL
+      process.env.DATABASE_URL
     : process.env.DATABASE_URL;
 
 const options = {
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { GraphQLSchema } from 'graphql';
 import dotenv from 'dotenv';
 dotenv.config();
 
-// import models from '~/app/models';
+import models from '~/app/models';
 
 const isDevelop = process.env.NODE_ENV === 'dev';
 
@@ -14,7 +14,7 @@ var options = {
 };
 
 // Font: https://www.npmjs.com/package/sequelize-graphql-schema
-// const { generateSchema } = require('sequelize-graphql-schema')(options);
+const { generateSchema } = require('sequelize-graphql-schema')(options);
 
 const app = express();
 // app.use(cors());
@@ -24,19 +24,19 @@ app.get('/api', (req, res) => {
   res.send({ status: 'ok' });
 });
 
-// app.use(
-//   '/graphql',
-//   graphqlHTTP({
-//     schema: new GraphQLSchema(generateSchema(models)),
-//     // graphiql: isDevelop
-//     graphiql: true
-//   })
-// );
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: new GraphQLSchema(generateSchema(models)),
+    // graphiql: isDevelop
+    graphiql: true
+  })
+);
 
 const port = process.env.PORT || 80;
 
 console.log('');
-console.log('INICIO! ' + port);
+console.log('START ' + port);
 console.log('');
 
 app.listen(port, () => {
